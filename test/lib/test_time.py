005e54b2e4031727768243a3b71e2fba21672f47
Sarbjit Singh <srbcheema1@gmail.com>
2018-10-02 22:25:17 +0530

    improved directory structure of tests

--- test/lib/test_time.py
+++ test/lib/test_time.py
@@ -4,3 +4,6 @@ from medipack.lib.time import Time
 
 def test_get_time():
     assert Time.get_time('0:5') == '00:00:05'
+
+def test_relative_time():
+    assert Time.get_relative_time('0:5','0:15') == '00:00:10'
